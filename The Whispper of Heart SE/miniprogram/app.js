566fd0d3cd9b4db309485b2a10f894ad7563a2da
fengyj15 <18613263020@163.com>
2018-12-05 23:38:37 +0800

    添加了所有会用到的页面的空白页面，在页面的wxml文件的注释中说明了每个页面的功能

--- The Whispper of Heart SE/miniprogram/app.js
+++ The Whispper of Heart SE/miniprogram/app.js
@@ -11,5 +11,26 @@ App({
     }
 
     this.globalData = {}
+  },
+    getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res);
+              that.globalData.encryptedData = res.encryptedData;
+              that.globalData.iv = res.iv;
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
   }
 })
