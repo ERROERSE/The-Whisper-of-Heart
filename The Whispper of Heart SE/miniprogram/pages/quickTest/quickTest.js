0387ac34f71674f15d87f3d7f8e4f0c124e36503
wanjp15 <wanjp15@mails.tsinghua.edu.cn>
2019-01-04 16:28:01 +0800

    这是最终版前最后一版

--- The Whispper of Heart SE/miniprogram/pages/quickTest/quickTest.js
+++ The Whispper of Heart SE/miniprogram/pages/quickTest/quickTest.js
@@ -1,13 +1,12 @@
 // miniprogram/pages/quickTest/quickTest.js
 const app = getApp()
-
 Page({
   data: {
     selectTest: true,
     selectArea: false,
     testId: 0,
-    testType: ['请选择一个测试方面','躯体健康测试', '强迫症测试', '人际关系敏感程度测试', '抑郁程度测试', '焦虑程度测试',
-    '敌对程度测试','恐惧程度测试', '偏执程度测试','精神病性程度测试'],
+    testType: ['请选择一个测试方面', '躯体健康测试', '强迫症测试', '人际关系敏感程度测试', '抑郁程度测试', '焦虑程度测试',
+      '敌对程度测试', '恐惧程度测试', '偏执程度测试', '精神病性程度测试'],
   },
   //点击选择类型
   clickTest: function () {
@@ -31,29 +30,37 @@ Page({
       selectTest: true,
       selectArea: false,
     })
-    app.globalData.testIndex=this.data.testId
+    app.globalData.testIndex = this.data.testId
     console.log(app.globalData.testIndex)
   },
 
   begin: function () {
-    wx.redirectTo({
-      url: '../quickTest/quick',
-    })
-  },
+    if (this.data.testId != 0) {
+      wx.redirectTo({
+        url: '../quickTest/quick',
+      })
+    }
+    else {
+  wx.showToast({
+    icon: 'none',
+    title: '请选择一个测试方面'
+  })
+}
+},
 
-  onLoad: function (options) {
-    // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady: function () {
-    // 页面渲染完成
-  },
-  onShow: function () {
-    // 页面显示
-  },
-  onHide: function () {
-    // 页面隐藏
-  },
-  onUnload: function () {
-    // 页面关闭
-  }
+onLoad: function (options) {
+  // 页面初始化 options为页面跳转所带来的参数
+},
+onReady: function () {
+  // 页面渲染完成
+},
+onShow: function () {
+  // 页面显示
+},
+onHide: function () {
+  // 页面隐藏
+},
+onUnload: function () {
+  // 页面关闭
+}
 })
